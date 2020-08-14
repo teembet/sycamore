6bbdee8b6fccabe59451e4bebdc7549594f8a8bb
teembet <51920737+teembet@users.noreply.github.com>
2020-08-14 11:00:03 +0100

    First commit

---
+++ src/components/footer.vue
@@ -0,0 +1,246 @@
+<template>
+  <div>
+    <div class="footer">
+      <div class="contain">
+        <div class="col-large">
+          <img src="../assets/footer.png" alt="" />
+          <p>
+            At Sycamore, our primary objective is to create more opportunities
+            to get you financially empowered. Follow us on social media
+            Platforms, as we make this objective a reality.
+          </p>
+          <br />
+          <v-btn
+            depressed
+            style=" text-transform:none;border-radius:20px;"
+            dark
+            color="#63C132"
+            >Download our brochure
+            <v-icon style="margin-left:10px;font-size:13px" dark
+              >mdi-arrow-right</v-icon
+            ></v-btn
+          >
+        </div>
+        <div class="col">
+          <h1>Company</h1>
+          <ul>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>FAQ</li>
+            <li>Blog</li>
+            <li>Food Store</li>
+          </ul>
+        </div>
+        <div class="col">
+          <h1>Products</h1>
+          <ul>
+            <li>Invest</li>
+            <li>Borrow</li>
+            <li>Advisory</li>
+          </ul>
+        </div>
+        <div class="col">
+          <h1>Legal</h1>
+          <ul>
+            <li>Privacy Policy</li>
+            <li>Terms</li>
+          </ul>
+        </div>
+        <div class="col">
+          <h1>Company</h1>
+          <ul>
+            <li>
+              <v-icon style="font-size:20px;margin-right:5px;" color="#63C132"
+                >mdi-map-marker</v-icon
+              >15, Modupe Johnson Crescent,Surulere, Lagos.
+            </li>
+            <li>
+              <v-icon style="font-size:20px;margin-right:5px;" color="#63C132"
+                >mdi-phone-plus</v-icon
+              >+2348097341810
+            </li>
+          </ul>
+        </div>
+        <div class="clearfix"></div>
+      </div>
+    </div>
+    <v-footer dark padless>
+      <v-card class="flex" flat tile>
+        <v-card-title style="background:#4D4D4D">
+          <strong style="width:800px;font-size:12px" class="subheading"
+            >Sycamore is authorized and regulated by the Money Lenders Act Cap
+            52. The Sycamore brand is a property of Sycamore Integrated
+            Solutions Limited. © Copyrights, All right reserved by
+            Sycamore</strong
+          >
+
+          <v-spacer></v-spacer>
+
+          <v-btn v-for="icon in icons" :key="icon" class="mx-4" dark icon>
+            <v-icon size="24px">{{ icon }}</v-icon>
+          </v-btn>
+        </v-card-title>
+      </v-card>
+    </v-footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "footer",
+};
+</script>
+
+<style>
+.footer {
+  width: 100%;
+  position: relative;
+  height: 400px;
+  background-color: #e8f2e9;
+  padding: 50px 30px;
+}
+.footer .col {
+  width: 15%;
+  height: auto;
+  float: left;
+  box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  padding: 0px 20px 20px 20px;
+}
+.footer .col-large {
+  width: 40%;
+  height: auto;
+  float: left;
+  box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  padding: 0px 20px 20px 20px;
+}
+.footer .col-large h1 {
+  margin-bottom: 10px;
+  padding: 0;
+  font-family: inherit;
+  font-size: 13px;
+  line-height: 19px;
+  padding: 20px 0px 5px 0px;
+  color: rgb(26, 26, 26);
+  font-weight: bold;
+  text-transform: uppercase;
+  letter-spacing: 0.25em;
+}
+.footer .col-large p {
+  margin: 0;
+  padding: 0;
+  font-family: inherit;
+  font-size: 13px;
+  line-height: 19px;
+  padding: 20px 0px 5px 0px;
+  color: #002d04;
+  letter-spacing: 0.1em;
+}
+.footer .col h1 {
+  margin-bottom: 20px;
+  padding: 0;
+  font-family: inherit;
+  font-size: 13px;
+  line-height: 17px;
+  padding: 20px 0px 5px 0px;
+  color: #002d04;
+  font-weight: bold;
+  text-transform: uppercase;
+  letter-spacing: 0.25em;
+}
+.footer .col ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+.footer .col ul li {
+  color: #1d1d1d;
+  font-size: 13px;
+  font-family: inherit;
+  padding: 5px 0px 5px 0px;
+  cursor: pointer;
+  transition: 0.2s;
+  -webkit-transition: 0.2s;
+  -moz-transition: 0.2s;
+}
+.social ul li {
+  display: inline-block;
+  padding-right: 5px !important;
+}
+
+.footer .col ul li:hover {
+  color: #df3b3b;
+  transition: 0.1s;
+  -webkit-transition: 0.1s;
+  -moz-transition: 0.1s;
+}
+.clearfix {
+  clear: both;
+}
+.store {
+  color: white;
+}
+.social-media {
+  color: white;
+}
+.social-media-store {
+  padding: 0 5%;
+}
+.footer-bottom-div {
+  border-top: 1px solid white;
+  border-bottom: 1px solid white;
+  text-align: center;
+  display: block;
+  color: white;
+  background: #252525;
+  height: 70px;
+  &-text {
+    line-height: 70px;
+  }
+}
+@media only screen and (min-width: 1280px) {
+  .contain {
+    width: 1200px;
+    margin: 0 auto;
+  }
+}
+@media only screen and (max-width: 1139px) {
+  .contain .social {
+    width: 1000px;
+    display: block;
+  }
+  .social h1 {
+    margin: 0px;
+  }
+}
+@media only screen and (max-width: 950px) {
+  .footer .col {
+    width: 33%;
+  }
+  .footer .col h1 {
+    font-size: 14px;
+  }
+  .footer .col ul li {
+    font-size: 13px;
+  }
+}
+@media only screen and (max-width: 500px) {
+  .footer .col {
+    width: 100%;
+  }
+  .footer .col h1 {
+    font-size: 14px;
+  }
+  .footer .col ul li {
+    font-size: 13px;
+  }
+}
+@media only screen and (max-width: 340px) {
+  .footer .col {
+    width: 100%;
+  }
+}
+</style>
---
+++ src/components/home.vue
@@ -0,0 +1,284 @@
+<template>
+  <div class="home">
+    <v-carousel hide-delimiters height="470">
+      <v-carousel-item v-for="(item, i) in items" :key="i" :src="item.src">
+        <div class="slider">
+          <div class="text-content">
+            <h1 style="width:470px;font-size:37px">
+              You deserve the very best!
+            </h1>
+            <p style="font-size:14px;">
+              You can get it just now, then pay later
+            </p>
+          </div>
+        </div>
+      </v-carousel-item>
+    </v-carousel>
+    <br />
+    <v-row>
+      <v-col cols="6" md="3">
+        <v-card class="option-card">
+          <v-row style="padding: 0 20px 0 20px">
+            <v-col cols="3">
+              <img
+                style="margin-top:0"
+                height="40"
+                src="../assets/option1.png"
+                alt=""
+              />
+            </v-col>
+            <v-col cols="9" style="line-height:40px;font-weight:bold">
+              Select product
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="3">
+        <v-card class="option-card">
+          <v-row style="padding: 0 20px 0 20px">
+            <v-col cols="3">
+              <img
+                style="margin-top:0"
+                height="40"
+                src="../assets/option2.png"
+                alt=""
+              />
+            </v-col>
+            <v-col cols="9" style="line-height:40px;font-weight:bold">
+              Upload product
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="3">
+        <v-card class="option-card">
+          <v-row style="padding: 0 20px 0 20px">
+            <v-col cols="3">
+              <img
+                style="margin-top:0"
+                height="40"
+                src="../assets/option3.png"
+                alt=""
+              />
+            </v-col>
+            <v-col cols="9" style="line-height:40px;font-weight:bold">
+              Pay 30% Upfront
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+      <v-col cols="6" md="3">
+        <v-card class="option-card">
+          <v-row style="padding: 0 20px 0 20px">
+            <v-col cols="3">
+              <img
+                style="margin-top:0"
+                height="40"
+                src="../assets/option4.png"
+                alt=""
+              />
+            </v-col>
+            <v-col cols="9" style="line-height:40px;font-weight:bold">
+              Get delivery in 2 days
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+    <br />
+    <div style="text-align:center;margin-top:20px;margin-bottom:20px;">
+      <h2 class="section-header">Product Category</h2>
+    </div>
+    <div style="padding:20px">
+      <v-row>
+        <v-col
+          cols="6"
+          v-for="item in category"
+          :key="item"
+          style="padding:30px 20px 0 20px"
+        >
+          <v-card
+            :color="item.color"
+            flat
+            style="height:280px;border-radius:15px"
+          >
+            <v-row style="padding: 0 20px 0 20px">
+              <v-col cols="6" style="padding-top:70px; padding-left:30px">
+                <h2 style="font-size:30px;color:white">{{ item.title }}</h2>
+                <br />
+                <v-btn
+                  outlined
+                  color="white"
+                  depressed
+                  style="text-transform:none;"
+                  dark
+                  >Buy Now
+                  <v-icon style="margin-left:10px;font-size:20px" dark
+                    >mdi-arrow-right</v-icon
+                  ></v-btn
+                >
+              </v-col>
+              <v-col cols="6" style="line-height:40px;font-weight:bold">
+                <img :src="item.img" alt="" />
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+        <v-col cols="6" style="padding:30px 20px 0 20px">
+          <v-card color="#EEF0FF" flat style="height:280px;border-radius:15px">
+            <v-row style="padding: 0 20px 0 20px">
+              <v-col cols="6" style="padding-top:70px;padding-left:30px">
+                <h2 style="font-size:30px;color:black">Air Conditioner</h2>
+                <br />
+                <v-btn
+                  outlined
+                  color="black"
+                  depressed
+                  style="text-transform:none;"
+                  >Buy Now
+                  <v-icon style="margin-left:10px;font-size:20px" dark
+                    >mdi-arrow-right</v-icon
+                  ></v-btn
+                >
+              </v-col>
+              <v-col
+                cols="6"
+                style="text-align:center;line-height:40px;font-weight:bold"
+              >
+                <img src="../assets/ac.png" alt="" />
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+      </v-row>
+      <br /><br /><br />
+      <div style="text-align:center">
+        <v-btn
+          x-large
+          style="font-weight:bold;text-transform:none;"
+          outlined
+          color="black"
+          >See all Products</v-btn
+        >
+      </div>
+    </div>
+
+    <div class="botttom-banner">
+      <h2 style="font-size:30px;color:#E23939">How it works</h2>
+      <br />
+      <div style="width:600px">
+        <li>Available to Lagos Residents only</li>
+        <li>Available to salaried workers</li>
+        <li>
+          30% upfront payment must be received before items are delivered. 70%
+          balance to be paid up on your next salary pay day
+        </li>
+        <li>
+          Applicable interest rate as indicated is indicated on each basket.
+        </li>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+
+  data() {
+    return {
+      items: [
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+        },
+      ],
+      category: [
+        {
+          color: "#BD1E4A",
+          title: "Phones, Tablet and Laptop",
+          img: require("../assets/lappy.png"),
+        },
+        {
+          color: "#1B1333",
+          title: "Television Set",
+          img: require("../assets/tv.png"),
+        },
+        {
+          color: "#747C98",
+          title: "Refrigerators and Freezers",
+          img: require("../assets/fridgee.png"),
+        },
+        {
+          color: "#303030",
+          title: "Gas Cookers",
+          img: require("../assets/oven.png"),
+        },
+        {
+          color: "#532010",
+          title: "Generators",
+          img: require("../assets/gen.png"),
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+.botttom-banner {
+  margin-top: 50px;
+  overflow: hidden;
+  border-radius: 15px;
+  padding: 50px 80px 50px 80px;
+  background-image: url("../assets/Nav.png");
+  background-size: cover;
+}
+.section-header {
+  display: block;
+  font-size: 20px;
+  position: relative;
+}
+.section-header::before {
+  width: 40%;
+  display: block;
+  position: absolute;
+  content: "";
+  background: black;
+  height: 1px;
+  left: 0;
+  top: 50%;
+}
+.section-header::after {
+  width: 40%;
+  display: block;
+  position: absolute;
+  content: "";
+  background: black;
+  height: 1px;
+  right: 0;
+  top: 50%;
+}
+.option-card-content {
+  line-height: 60px;
+}
+.option-card {
+  height: 60px;
+}
+.text-content {
+}
+.slider {
+  padding: 180px 100px;
+}
+.home {
+  padding: 50px;
+}
+</style>
